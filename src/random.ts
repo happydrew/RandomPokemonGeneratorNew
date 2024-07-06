e14ab960226f88316ba066c74607fcf8a9bafa57
zhugechina <zhugetd@126.com>
2024-07-06 13:07:29 +0800

    去掉不用的第三方css引用，页脚改成静态的

--- src/random.ts
+++ src/random.ts
@@ -57,7 +57,7 @@ function onPageLoad() {
     addNumrangeValidateListeners();
     addClickTipListeners();
     loadPokemonDetailsFromCache()
-    displayYearsInFooter();
+    //displayYearsInFooter();
     clearOldCacheVersion();
 }
 
